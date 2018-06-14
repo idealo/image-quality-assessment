87971c06d7ce115b2886a97e9efebeb6d9caca69
clennan <christopherlennan@gmail.com>
2018-06-14 15:20:08 +0200

    Fix environment variable mapping in docker container.

--- train-local.sh
+++ train-local.sh
@@ -21,7 +21,7 @@ cp $SAMPLES_FILE $TRAIN_JOB_DIR/samples.json
 DOCKER_RUN="docker run -d
   -v $IMAGE_DIR:/src/images
   -v "$(pwd)/$TRAIN_JOB_DIR":/src/$TRAIN_JOB_DIR
-  -e TRAIN_JOB_DIR=/src/$TRAIN_JOB_DIR
+  -e TRAIN_JOB_DIR=$TRAIN_JOB_DIR
   $docker_image"
 
 eval $DOCKER_RUN
