596dd2141435cde069a396e41a70d35b5aba919e
clennan <christopherlennan@gmail.com>
2018-06-14 16:01:43 +0200

    Remove not needed numpy earth mover distance.

--- src/utils/losses.py
+++ src/utils/losses.py
@@ -1,5 +1,4 @@
 
-import numpy as np
 from keras import backend as K
 
 
@@ -8,10 +7,3 @@ def earth_movers_distance(y_true, y_pred):
     cdf_pred = K.cumsum(y_pred, axis=-1)
     emd = K.sqrt(K.mean(K.square(cdf_true - cdf_pred), axis=-1))
     return K.mean(emd)
-
-
-def earth_movers_distance_np(y_true, y_pred):
-    cdf_true = np.cumsum(y_true, axis=-1)
-    cdf_pred = np.cumsum(y_pred, axis=-1)
-    emd = np.sqrt(np.mean(np.square(cdf_true - cdf_pred), axis=-1))
-    return np.mean(emd)
