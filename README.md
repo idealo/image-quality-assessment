20f717aa158fc3f182c025cebb34804761145a5b
clennan <christopherlennan@gmail.com>
2018-11-26 10:37:33 +0100

    Move tfs contribution out of src. Update README and sample client.

--- README.md
+++ README.md
@@ -191,18 +191,25 @@ For the AVA dataset we randomly assigned 90% of samples to the train set, and 10
 
 ## Serving NIMA with TensorFlow Serving
 TensorFlow versions of both the technical and aesthetic MobileNet models are provided,
-along with the script to generate them from the original Keras files, under the `src/contrib/tf_serving` directory.
+along with the script to generate them from the original Keras files, under the `contrib/tf_serving` directory.
 
 There is also an already configured TFS `Dockerfile` that you can use.
 
-Build the NIMA TFS container: 
-1. cd into `src/contrib/tf_serving`
-2. run `docker build -t tfs_nima .`
+To get predictions from the aesthetic or technical model:
+1. Build the NIMA TFS Docker image `docker build -t tfs_nima contrib/tf_serving`
+2. Run a NIMA TFS container with `docker run -d --name tfs_nima -p 8500:8500 tfs_nima`
+3. Install python dependencies to run TF serving sample client
+```
+virtualenv -p python3 contrib/tf_serving/venv_tfs_nima
+source contrib/tf_serving/venv_tfs_nima/bin/activate
+pip install -r contrib/tf_serving/requirements.txt
+```
+3. Get predictions from aesthetic or technical model by running the sample client
+```
+python -m contrib.tf_serving.tfs_sample_client --image-path src/tests/test_images/42039.jpg --model-name mobilenet_aesthetic
+python -m contrib.tf_serving.tfs_sample_client --image-path src/tests/test_images/42039.jpg --model-name mobilenet_technical
+```
 
-Run the NIMA TFS container with:
-1. `docker run -d --name tfs_nima -p 8500:8500 tfs_nima`
-
-You can use the `tfs_sample_client.py` script to get predictions from the TFS models.
 
 ## Maintainers
 * Christopher Lennan, github: [clennan](https://github.com/clennan)
--- contrib/tf_serving/tfs_sample_client.py
+++ contrib/tf_serving/tfs_sample_client.py
@@ -1,16 +1,14 @@
-from __future__ import absolute_import, division, print_function
-
-import tensorflow as tf
+import json
+import argparse
 import keras
 import numpy as np
-import skimage
-from scipy import misc
+import tensorflow as tf
+from src.utils import utils
 from grpc.beta import implementations
 from tensorflow_serving.apis import predict_pb2, prediction_service_pb2
 
 TFS_HOST = 'localhost'
 TFS_PORT = 8500
-MODEL_NAME = 'mobilenet_technical'
 
 
 def normalize_labels(labels):
@@ -20,28 +18,36 @@ def normalize_labels(labels):
 
 def calc_mean_score(score_dist):
     score_dist = normalize_labels(score_dist)
-    return (score_dist*np.arange(1, 11)).sum()
+    return (score_dist * np.arange(1, 11)).sum()
 
 
-def get_image_quality_predictions(image_path):
+def get_image_quality_predictions(image_path, model_name):
     # Load and preprocess image
-    image = skimage.io.imread(image_path)
-    image = misc.imresize(image, (224, 224), interp='nearest')
-    image = np.float32(image)
+    image = utils.load_image(image_path, target_size=(224, 224))
     image = keras.applications.mobilenet.preprocess_input(image)
 
     # Run through model
     channel = implementations.insecure_channel(TFS_HOST, TFS_PORT)
     stub = prediction_service_pb2.beta_create_PredictionService_stub(channel)
     request = predict_pb2.PredictRequest()
-    request.model_spec.name = MODEL_NAME
+    request.model_spec.name = model_name
     request.model_spec.signature_name = 'image_quality'
 
     request.inputs['input_image'].CopyFrom(
-        tf.contrib.util.make_tensor_proto(np.expand_dims(image, 0)))
+        tf.contrib.util.make_tensor_proto(np.expand_dims(image, 0))
+    )
 
     response = stub.Predict(request, 10.0)
     result = round(calc_mean_score(response.outputs['quality_prediction'].float_val), 2)
 
-    return result
+    print(json.dumps({'mean_score_prediction': np.round(result, 3)}, indent=2))
 
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-ip', '--image-path', help='Path to image file.', required=True)
+    parser.add_argument(
+        '-mn', '--model-name', help='mobilenet_aesthetic or mobilenet_technical', required=True
+    )
+    args = parser.parse_args()
+    get_image_quality_predictions(**args.__dict__)
